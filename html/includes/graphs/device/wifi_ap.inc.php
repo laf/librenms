6ca08fa1a5f28e89c501b6b6008e2897b7ffa552
laf <neil@lathwood.co.uk>
2017-01-25 17:34:13 +0000

    more updates

--- html/includes/graphs/device/wifi_ap.inc.php
+++ html/includes/graphs/device/wifi_ap.inc.php
@@ -2,16 +2,27 @@
 
 $ap = dbFetchRow("SELECT * FROM `wifi_aps` WHERE `device_id` = ? AND `id` = ? AND `essid` = ?", array($device['device_id'], $vars['id'], $vars['essid']));
 $rrd_filename = rrd_name($device['hostname'], "unifi-ap-traffic-". $ap['id']. "-" . $ap['essid']);
-$rrd_list[0] =
-        array(
-            'filename' => $rrd_filename,
-            'ds' => 'rx_bytes',
-            'descr' => "Rx Bytes",
-        );
-$rrd_list[1] =
-        array(
-            'filename' => $rrd_filename,
-            'ds' => 'tx_bytes',
-            'descr' => "Tx Bytes",
-        );
-require 'includes/graphs/generic_multi_line.inc.php';
+$i=0;
+        $rrd_list[$i]['filename']  = $rrd_filename;
+        $rrd_list[$i]['descr_in']  = 'Rx Bytes';
+        $rrd_list[$i]['descr_out'] = 'Tx Bytes';
+        $rrd_list[$i]['ds_in']     = 'rx_bytes';
+        $rrd_list[$i]['ds_out']    = 'tx_bytes';
+$units       = 'b';
+$total_units = 'B';
+$colours_in  = 'greens';
+$multiplier  = '1';
+$colours_out = 'blues';
+
+// $nototal = 1;
+$ds_in  = 'rx_bytes';
+$ds_out = 'tx_bytes';
+
+$graph_title .= '::bits';
+
+$colour_line_in  = '006600';
+$colour_line_out = '000099';
+$colour_area_in  = '91B13C';
+$colour_area_out = '8080BD';
+
+require 'includes/graphs/generic_multi_seperated.inc.php';
