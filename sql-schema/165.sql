677e8d6f7bf0231d7d3ce4d49ef9d42ef8d5a4b0
Neil Lathwood <neil@lathwood.co.uk>
2017-02-15 00:02:44 +0000

    updated

--- sql-schema/165.sql
+++ sql-schema/165.sql
@@ -1,5 +1,7 @@
 ALTER TABLE `applications` ADD INDEX(`device_id`);
 ALTER TABLE `alert_schedule` ADD INDEX(`recurring`);
 ALTER TABLE `alert_rules` ADD INDEX(`disabled`);
-ALTER TABLE `alert_map` ADD INDEX(`target`);
-ALTER TABLE `alert_map` ADD INDEX(`rule`);
+ALTER TABLE `ipv4_addresses` ADD INDEX(`ipv4_address`);
+ALTER TABLE `notifications_attribs` ADD INDEX( `key`, `value`);
+ALTER TABLE `loadbalancer_rservers` ADD INDEX(`device_id`);
+ALTER TABLE `pseudowires` ADD INDEX(`device_id`);
